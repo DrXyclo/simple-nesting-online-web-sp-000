f7acd5c032d3264a153381ba9f0efbd35724cc36
Dr. XyClo <sdcarmody@gmail.com>
2019-01-09 15:13:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,6 +111,7 @@ def changing_alan
       alans_new_info = "GUI"
        #  1==1
      programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_to_dennis
